6adc3cb358e9e1dd7a16237cc5e87397aac363b0
Shriram0463 <141933845+Shriram0463@users.noreply.github.com>
2024-06-26 18:04:21 +0530

    Add files via upload

---
+++ SSE_Streaming/Springboot/src/main/java/com/example/sentencestreaming/SentenceStreamingApplication.java
@@ -0,0 +1,12 @@
+package com.example.sentencestreaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentenceStreamingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SentenceStreamingApplication.class, args);
+    }
+}
